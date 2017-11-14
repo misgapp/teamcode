2c246397a68db3dbcbfb66917292908850d5d81c
carmel-yassour <carmel.yassour@gmail.com>
2017-11-14 20:46:19 +0200

    Cleanup unused imports

--- HardwareApollo.java
+++ HardwareApollo.java
@@ -29,12 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 
 public class HardwareApollo
 {
