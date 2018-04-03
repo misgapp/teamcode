7f9f2587bac83825b80d7d1e21c4adce9fe258e3
carmel-yassour <carmel.yassour@gmail.com>
2018-04-03 20:51:51 +0300

    Add more cubes for wall

--- AutoMain.java
+++ AutoMain.java
@@ -72,7 +72,11 @@ public abstract class AutoMain extends LinearOpMode {
         //reportImage(column);
         moveToCryptoBox(isRed, isCorner, column);
         putCube(isCorner);
-        moreCubs(isCorner, column);
+        if (isCorner) {
+            moreCubsCorner(column);
+        } else {
+            moreCubsWall(column, isRed);
+        }
     }
 
     //Set the claws & lift to right position, grab cube
@@ -340,97 +344,165 @@ public abstract class AutoMain extends LinearOpMode {
     }
 
     // Put more cube in crypto box
-    public void moreCubs(boolean isCorner, RelicRecoveryVuMark column){
+    public void moreCubsCorner(RelicRecoveryVuMark column){
         boolean grabCube = false;
-        if (isCorner){
-            robot.openClaws();
-            startLift(1100);
-            gyroDrive(speed, -1650, -90);
-            //encoderLift(1 , 1100);
-            gyroTurn(speed, 90);
-            //gyroHold(speed, 90, 0.7);
-            robot.halfCloseClaws();
-            //P_DRIVE_COEFF = 0.17;
-            robot.setPositionWheel(robot.GRAB_POSITION);
-            int ticks = gyroDrive(speed, 2600, 90, true, true);
 
-            if ((robot.sensorDistanceDown.getDistance(DistanceUnit.CM) < 15
-                    && robot.sensorDistanceUp.getDistance(DistanceUnit.CM) < 15)
-                    || robot.sensorDistanceUp.getDistance(DistanceUnit.CM) < 15){
-                // If got two cubes or only upper cube.
-                grabCube = true;
-                robot.closeClaws();
+        robot.openClaws();
+        startLift(1100);
+        gyroDrive(speed, -1650, -90);
+        //encoderLift(1 , 1100);
+        gyroTurn(speed, 90);
+        //gyroHold(speed, 90, 0.7);
+        robot.halfCloseClaws();
+        //P_DRIVE_COEFF = 0.17;
+        robot.setPositionWheel(robot.GRAB_POSITION);
+        int ticks = gyroDrive(speed, 2600, 90, true, true);
+
+        if ((robot.sensorDistanceDown.getDistance(DistanceUnit.CM) < 15 && robot.sensorDistanceUp.getDistance(DistanceUnit.CM) < 15) || robot.sensorDistanceUp.getDistance(DistanceUnit.CM) < 15) {
+            // If got two cubes or only upper cube.
+            grabCube = true;
+            robot.closeClaws();
+            sleep(100);
+            //startLift(-2000);
+            encoderLift(1, -2000);
+            gyroDrive(speed, -300, 90);
+            ticks -= 300;
+            goUpSpin = false; // Spin the claws to drop third cube.
+            gyroDrive(speed, -ticks + 400, 90);
+
+        } else if (robot.sensorDistanceDown.getDistance(DistanceUnit.CM) < 15) {
+            // If got only lower go back, spin and try again.
+            grabCube = true;
+            if (overallTimer.seconds() <= 16) {
+                robot.closeClawsDown();
                 sleep(100);
-                //startLift(-2000);
+                startLift(-2000);
+                //encoderLift(1, -2000);
+                gyroDrive(speed, -400, 90);
+                ticks -= 400;
+                robot.spinner.setPower(0.8);
+                ElapsedTime runtime = new ElapsedTime();
+                runtime.reset();
+                while (opModeIsActive() && robot.touchSpinnerDown.getState() && runtime.seconds() < 2) {
+                    handleLift();
+                    idle();
+                }
+                robot.spinner.setPower(0);
+                goUpSpin = false;
+                encoderLift(1, 2000);
+                ticks += gyroDrive(speed, 1400, 90, true, false);
+                robot.closeClaws();
                 encoderLift(1, -2000);
                 gyroDrive(speed, -300, 90);
                 ticks -= 300;
-                goUpSpin = false; // Spin the claws to drop third cube.
-                gyroDrive(speed, -ticks +400, 90);
-
-            } else if (robot.sensorDistanceDown.getDistance(DistanceUnit.CM) < 15){
-                // If got only lower go back, spin and try again.
-                grabCube = true;
-                if (overallTimer.seconds() <= 16) {
-                    robot.closeClawsDown();
-                    sleep(100);
-                    startLift(-2000);
-                    //encoderLift(1, -2000);
-                    gyroDrive(speed, -400, 90);
-                    ticks -= 400;
-                    robot.spinner.setPower(0.8);
-                    ElapsedTime runtime = new ElapsedTime();
-                    runtime.reset();
-                    while (opModeIsActive() && robot.touchSpinnerDown.getState() && runtime.seconds() < 2) {
-                        handleLift();
-                        idle();
-                    }
-                    robot.spinner.setPower(0);
-                    goUpSpin = false;
-                    encoderLift(1, 2000);
-                    ticks += gyroDrive(speed, 1400, 90, true, false);
-                    robot.closeClaws();
-                    encoderLift(1, -2000);
-                    gyroDrive(speed, -300, 90);
-                    ticks -= 300;
-                    goUpSpin = true; // Spin the claws to drop third cube.
-                    gyroDrive(speed, -ticks + 400, 90);
-                } else {
-                    robot.closeClaws();
-                    sleep(100);
-                    encoderLift(1, -400);
-                    gyroDrive(speed, -ticks + 400, 90);
-                }
-            }
-
-            if (grabCube){
-                //gyroDrive(speed, -1700, 90);
-                robot.closeClaws();
-                robot.setPositionWheel(robot.STOP_POSITION);
-                //startLift(-3000);
-                double angle = (column == RelicRecoveryVuMark.RIGHT) ? -50 : -130;
-                gyroTurn(speed, angle);
-                //gyroHold(speed, 180, 1);
-                //sleep(100);
-
-                encoderLift(1, 400);
-                gyroDrive(speed, 1000, angle);
-                gyroTurn(speed, -90);
-                encoderLift(1, 1000);
-                gyroDrive(speed, 1100, -90);
-                robot.setPositionWheel(robot.DROP_POSITION);
-                sleep(650);
-                //gyroDrive(speed, -150, -90);
-                driveStrait(speed, 600);
-                //robot.openClaws();
-                driveStrait(speed, -450);
-                robot.setPositionWheel(robot.STOP_POSITION);
-                robot.openClaws();
+                goUpSpin = true; // Spin the claws to drop third cube.
+                gyroDrive(speed, -ticks + 400, 90);
             } else {
-                gyroDrive(speed, -ticks -500, 90);
+                robot.closeClaws();
+                sleep(100);
+                encoderLift(1, -400);
+                gyroDrive(speed, -ticks + 400, 90);
             }
         }
+
+        if (grabCube) {
+            //gyroDrive(speed, -1700, 90);
+            robot.closeClaws();
+            robot.setPositionWheel(robot.STOP_POSITION);
+            //startLift(-3000);
+            double angle = (column == RelicRecoveryVuMark.RIGHT) ? -50 : -130;
+            gyroTurn(speed, angle);
+            //gyroHold(speed, 180, 1);
+            //sleep(100);
+
+            encoderLift(1, 400);
+            gyroDrive(speed, 1000, angle);
+            gyroTurn(speed, -90);
+            encoderLift(1, 1000);
+            gyroDrive(speed, 1100, -90);
+            robot.setPositionWheel(robot.DROP_POSITION);
+            sleep(650);
+            //gyroDrive(speed, -150, -90);
+            driveStrait(speed, 600);
+            //robot.openClaws();
+            driveStrait(speed, -450);
+            robot.setPositionWheel(robot.STOP_POSITION);
+            robot.openClaws();
+        } else {
+            gyroDrive(speed, -ticks - 500, 90);
+        }
     }
+
+    public void moreCubsWall(RelicRecoveryVuMark column, boolean isRed){
+        robot.openClaws();
+        encoderLift(1, 1000);
+        gyroDrive(speed, -300, 0);
+
+        // Default values for right for red or left for blue.
+        double angleToCubes = -45;
+        double angleToBox = 0;
+        int ticksToMeetingPoint = 2000;
+        int ticksToBox = 1700;
+
+        if (column == RelicRecoveryVuMark.CENTER) {
+            angleToCubes = -35;
+            ticksToMeetingPoint = 1600;
+            angleToBox = -7;
+            ticksToBox = 1700;
+        } else if (column == RelicRecoveryVuMark.LEFT && isRed ||
+                column == RelicRecoveryVuMark.RIGHT && !isRed) {
+            angleToCubes = -25;
+            ticksToMeetingPoint = 1300;
+            angleToBox = -30;
+            ticksToBox = 1700;
+        }
+
+        if (!isRed) {
+            angleToCubes += 180;
+            angleToCubes *= -1;
+            angleToBox += 180;
+            angleToBox *= -1;
+        }
+
+        gyroTurn(speed, angleToCubes);
+        gyroDrive(speed, -ticksToMeetingPoint, angleToCubes);
+        double grabAngle = isRed ? 160 : 20;
+        gyroTurn(speed, grabAngle);
+
+        robot.halfCloseClaws();
+
+        robot.setPositionWheel(robot.GRAB_POSITION);
+        int ticks = gyroDrive(speed, 3300, grabAngle, true, true);
+
+        robot.closeClaws();
+        sleep(100);
+        encoderLift(1, -2000);
+        gyroDrive(speed, -300, grabAngle);
+        ticks -= 300;
+        goUpSpin = false; // Spin the claws to drop third cube.
+        gyroDrive(speed, -ticks, grabAngle);
+
+        if (robot.sensorDistanceDown.getDistance(DistanceUnit.CM) < 15 ||
+                robot.sensorDistanceUp.getDistance(DistanceUnit.CM) < 15) {
+            gyroTurn(speed, angleToBox);
+            encoderLift(1, 1000);
+            gyroDrive(speed, ticksToBox, angleToBox);
+
+            // Drop cube.
+            encoderLift(1, 1000);
+            robot.setPositionWheel(robot.DROP_POSITION);
+            sleep(650);
+
+            driveStrait(speed, 600);
+
+            driveStrait(speed, -350);
+            robot.setPositionWheel(robot.STOP_POSITION);
+            robot.openClaws();
+        } else {
+            gyroDrive(speed, -1000, grabAngle);
+        }
+    }
+
 
     //Init vuforia
     public void initVuforia() {
